3d7d874edc6dcde116468489b334e8c0e3847130
Kamil Swiechowski <kamil.swiechowski@tivix.com>
2020-10-03 12:30:09 +0200

    New default template.

--- pkg/parse/rulesPage.go
+++ pkg/parse/rulesPage.go
@@ -7,15 +7,15 @@ type RulesPage struct {
 			Name  string `json:"name"`
 			File  string `json:"file"`
 			Rules []struct {
-				State    string `json:"state"`
-				Name     string `json:"name"`
-				Query    string `json:"query"`
-				Duration int    `json:"duration"`
-				Labels []interface{} `json:"labels"`
-				Annotations []interface{} `json:"annotations"`
-				Alerts []interface{} `json:"alerts"`
-				Health string        `json:"health"`
-				Type   string        `json:"type"`
+				State       string            `json:"state"`
+				Name        string            `json:"name"`
+				Query       string            `json:"query"`
+				Duration    int               `json:"duration"`
+				Labels      map[string]string `json:"labels"`
+				Annotations map[string]string `json:"annotations"`
+				Alerts      []interface{}     `json:"alerts"`
+				Health      string            `json:"health"`
+				Type        string            `json:"type"`
 			} `json:"rules"`
 			Interval int `json:"interval"`
 		} `json:"groups"`
--- pkg/tpl/template.go
+++ pkg/tpl/template.go
@@ -2,13 +2,22 @@ package tpl
 
 func DefaultTemplate() []byte {
 	return []byte(`
-{{ range .Data.Groups }}
-{{ .Name | upper }}
-{{ range .Rules }}
-{{ .Name }}
-Query: {{ .Query }}
-Duration: {{ .Duration }}
-{{ end }}
-{{ end }}
+{{- range .Data.Groups -}}
+# {{ .Name | upper }}
+{{- range .Rules }}
+## {{ .Name }}
+### Query:
+{{ .Query }}
+### Duration: {{ div .Duration 60 }}m
+### Labels:
+{{- range $key, $value :=  .Labels }}
+>  {{ $key }}: {{ $value }}
+{{- end }}
+### Annotations:
+{{- range $key, $value :=  .Annotations }}
+>  {{ $key }}: {{ $value }}
+{{- end }}
+{{- end }}
+{{- end }}
 `)
 }
